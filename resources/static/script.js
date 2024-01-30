53fd0f05147cba72c9be909912d47d4fd1601694
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-01-30 23:06:00 +0100

    new checkbox design, adding and removing elements

--- resources/static/script.js
+++ resources/static/script.js
@@ -1,15 +1,19 @@
-function addCheckboxAction(checkbox) {
+function addCheckboxAndRemoveAction(checkbox, remove) {
     checkbox.addEventListener("click", function () {
         checkbox.classList.toggle("active");
+    })
+    remove.addEventListener("click", function () {
+        remove.parentNode.remove();
     })
 }
 
 let checkboxes = document.getElementsByClassName("custom-checkbox");
+let removeButtons = document.getElementsByClassName("remove");
 
 for (let i = 0; i < checkboxes.length; i++) {
     let checkbox = checkboxes[i];
-
-    addCheckboxAction(checkbox);
+    let removeButton = removeButtons[i];
+    addCheckboxAndRemoveAction(checkbox, removeButton);
 }
 
 const list = document.getElementById("list");
@@ -17,13 +21,22 @@ const add = document.getElementById("add");
 const listItem = document.createElement("li");
 
 add.addEventListener("click", function () {
-    const listItem = document.createElement("li");
-    const checkbox = document.createElement("div");
-    const itemText = document.createElement("p");
-    checkbox.className = "custom-checkbox";
-    addCheckboxAction(checkbox);
-    itemText.textContent = "123";
-    listItem.append(checkbox);
-    listItem.append(itemText);
-    list.append(listItem);
+    let tmp = window.prompt("Item text");
+    if (tmp != null) {
+        const listItem = document.createElement("li");
+        const checkbox = document.createElement("div");
+        const itemText = document.createElement("p");
+        checkbox.className = "custom-checkbox";
+        addCheckboxAction(checkbox);
+        if (itemText.textContent != null) {
+            listItem.append(checkbox);
+            listItem.append(itemText);
+            list.append(listItem);
+            console.log(itemText.textContent)
+        }
+    }
 });
+
+function askItemName() {
+
+}
