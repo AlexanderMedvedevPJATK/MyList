b3527151bfeea306d1d89f8f93bbd24ae2e9ee14
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-01-31 15:18:30 +0100

    editing and deleting introduced, adding elements improved

--- resources/static/script.js
+++ resources/static/script.js
@@ -1,19 +1,50 @@
-function addCheckboxAndRemoveAction(checkbox, remove) {
+function addButtonsEventListeners(checkbox, remove, edit) {
+    const paragraph = edit.previousElementSibling;
+    let initialText;
     checkbox.addEventListener("click", function () {
         checkbox.classList.toggle("active");
     })
     remove.addEventListener("click", function () {
-        remove.parentNode.remove();
+        if (paragraph.isContentEditable) {
+            paragraph.removeAttribute("contenteditable");
+            paragraph.textContent = initialText;
+            edit.src = "edit.svg";
+            console.log("done")
+        } else {
+            remove.parentNode.remove();
+        }
+    })
+    edit.addEventListener("click", function () {
+        if (!edit.previousElementSibling.isContentEditable) {
+            initialText = paragraph.textContent;
+            paragraph.contentEditable = "true";
+            edit.src = "check2.svg";
+            paragraph.focus();
+
+            let range = document.createRange();
+            let text = paragraph.firstChild;
+            range.setStart(text, text.length);
+            range.setEnd(text, text.length);
+
+            let selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+        } else {
+            paragraph.removeAttribute("contenteditable");
+            edit.src = "edit.svg";
+        }
     })
 }
 
-let checkboxes = document.getElementsByClassName("custom-checkbox");
-let removeButtons = document.getElementsByClassName("remove");
+const checkboxes = document.getElementsByClassName("custom-checkbox");
+const removeButtons = document.getElementsByClassName("remove");
+const editButtons = document.getElementsByClassName("edit");
 
 for (let i = 0; i < checkboxes.length; i++) {
     let checkbox = checkboxes[i];
     let removeButton = removeButtons[i];
-    addCheckboxAndRemoveAction(checkbox, removeButton);
+    let editButton = editButtons[i];
+    addButtonsEventListeners(checkbox, removeButton, editButton);
 }
 
 const list = document.getElementById("list");
@@ -21,21 +52,25 @@ const add = document.getElementById("add");
 const listItem = document.createElement("li");
 
 add.addEventListener("click", function () {
-    let tmp = window.prompt("Item text");
-    if (tmp != null) {
-        const listItem = document.createElement("li");
-        const checkbox = document.createElement("div");
-        const itemText = document.createElement("p");
-        const removeButton = document.createElement("img");
-        removeButton.src = "cross.svg";
-        checkbox.className = "custom-checkbox";
-        itemText.textContent = tmp;
-        addCheckboxAndRemoveAction(checkbox, removeButton);
-        listItem.append(checkbox);
-        listItem.append(itemText);
-        listItem.append(removeButton);
-        list.append(listItem);
-    }
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("div");
+    const itemText = document.createElement("p");
+    const removeButton = document.createElement("img");
+    const editButton = document.createElement("img");
+    removeButton.src = "cross.svg";
+    removeButton.className = "remove";
+    editButton.src = "check2.svg";
+    editButton.className = "edit";
+    checkbox.className = "custom-checkbox";
+    itemText.contentEditable = "true";
+    itemText.textContent = "";
+    listItem.append(checkbox);
+    listItem.append(itemText);
+    listItem.append(editButton);
+    listItem.append(removeButton);
+    list.append(listItem);
+    itemText.focus();
+    addButtonsEventListeners(checkbox, removeButton, editButton);
 });
 
 function askItemName() {
